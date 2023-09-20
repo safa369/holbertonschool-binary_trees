5b8f2c9bce6bd9fb5366e6e87bcff69abc94a442
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-09-21 00:09:11 +0200

    function to mesure height of node

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height- a function that mesures the height of tree
+ * @tree: the tree to mesure their height.
+ * Return: the mesur.
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t s1, s2;
+if (tree == NULL)
+return (0);
+s1 =  binary_tree_height(tree->left);
+s2 =  binary_tree_height(tree->right);
+if (s1 > s2)
+return (s1 + 1);
+else
+return (s2 + 1);
+}
