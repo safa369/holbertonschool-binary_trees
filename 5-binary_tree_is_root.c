b2a526a4b78b26f136b45019359b1939842e9190
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-09-20 14:40:16 +0200

    function to check if node is a root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,8 +8,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 if (node == NULL)
 return (0);
-if (node->right != NULL && node->left != NULL)
-return (1);
-else
+if (node->parent != NULL)
 return (0);
+else
+return (1);
 }
