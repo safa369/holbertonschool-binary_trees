23aa03470936a41662a080740f1d4f444cfe36f5
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-09-20 12:13:35 +0200

    insert left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 binary_tree_t *newNode = binary_tree_node(parent, value);
 
 if (parent == NULL)
+{
+free(newNode);
 return (NULL);
+}
 if (parent->left != NULL)
 {
 newNode->n = value;
