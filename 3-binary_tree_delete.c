a832b159334768ef68a6d41ed8fc2d0fca306ff6
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-09-20 13:11:51 +0200

    function that deletes tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,8 +7,8 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 if (tree == NULL)
 return;
-free(tree->right);
-free(tree->left);
+binary_tree_delete(tree->right);
+binary_tree_delete(tree->left);
 free(tree);
 return;
 }
