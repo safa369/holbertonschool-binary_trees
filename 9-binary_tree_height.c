a09297faadee01e3243c955d79f7b4a7e54d80dd
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-09-21 11:51:19 +0200

    function to mesure height of node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,12 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t s1, s2;
-if (tree == NULL || (tree->left ==NULL && tree->right == NULL))
+if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 return (0);
 s1 = binary_tree_height(tree->left);
 s2 = binary_tree_height(tree->right);
 if (s1 > s2)
 return (s1 + 1);
 else
-return (s2 + 1 );
+return (s2 + 1);
 }
